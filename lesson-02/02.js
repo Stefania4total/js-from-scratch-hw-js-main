f9ffeb3ce848d98d0ed1fc1c748f61ef6e591e1d
Stefania4total <Steshata@gmail.com>
2025-11-14 19:57:33 +0300

    3

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -9,7 +9,7 @@
 
 let dice1 = Math.floor(Math.random() *6) +1
 let dice2 = Math.floor(Math.random() *6) +1
-isWinningDouble = (dice1===dice2 && dice1>3)
+let isWinningDouble = (dice1===dice2 && dice1>3)
 
 console.log('Первый бросок: ' + dice1)
 console.log('Второй бросок: ' + dice2)
